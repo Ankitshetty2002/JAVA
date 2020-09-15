21fe28eb49fadd4d97b3583325ebb4755fd121a9
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-09-15 21:22:02 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,20 @@
+public class Addition
+
+{
+
+    public static void main(String args[]) 
+  
+  {
+ 
+     int a=45;
+  
+     int b=35;
+ 
+     int sum=a+b;
+
+ 
+     System.out.println("Addition of a and b = " + sum);
+
+    }
+
+ }
