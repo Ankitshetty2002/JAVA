0d6828c55cc73a8d3e23f219633b31c60afe5c5e
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-09-15 21:36:07 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,15 +1,10 @@
-public class Addition{
-    public static void main(String args[]){
- 
-     int a=45;
-  
-     int b=35;
- 
-     int sum=a+b;
-
- 
-     System.out.println("Addition of a and b = " + sum);
-
+public class Addition 
+{
+    public static void main(String args[])
+    {
+        int a=45;
+        int b=35;
+        int sum=a+b;
+        System.out.println("Addition of a and b = " + sum);
     }
-
- }
+}
