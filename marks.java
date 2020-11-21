c664ba24007a62a0b7fa31bc6e253cc91470f9de
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-21 20:45:10 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Marks
+
+{
+
+    public static void main(String[] args)
+
+    {
+
+        int marks;
+
+        System.out.println("Enter the marks obtained: ");
+
+        Scanner scan= new Scanner(System.in);
+
+        marks =scan.nextInt();
+
+        if (marks>40)
+
+        {
+
+            System.out.println(" The student is PASSED");
+
+        }
+
+        else
+
+        {
+
+            System.out.println(" The student is FAILED");
+
+        }
+
+    }
+
+}
