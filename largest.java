d6d5c31d427f64e0fd1f615332b7b6fb5138748c
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-21 20:44:38 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+public class Marks
+
+{
+  
+    public static void main(String args[])
+
+    {
+
+        Scanner scan=new Scanner(System.in);
+
+      {
+
+         int n1,n2,n3;
+
+         System.out.println("Enter first number: ");
+
+         n1=scan.nextInt();
+
+         System.out.println("Enter second number: ");
+
+         n2=scan.nextInt();
+
+         System.out.println("Enter third number: ");
+
+         n3=scan.nextInt();
+
+         if (n1>n2)
+
+         {
+ 
+            if(n1>n3)
+
+             {
+
+              System.out.println(" The largest no. is "+n1);
+
+             }
+
+             else
+
+             {
+
+              System.out.println(" The largest no. is "+n3):
+
+             }
+
+         }
+
+         else if(n2>n3)
+
+         {
+
+             System.out.println(" The largest no. is "+n2);
+
+         }
+
+         else
+
+         {
+
+             System.out.println(" The largest no. is "+n3);
+
+         }
+
+      }
+
+    }
+
+}
