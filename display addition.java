873f134e3a180601f7a339096a80521e673cd8dd
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-21 20:43:10 +0530

    Add files via upload

---
+++ display addition.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Addition
+
+{
+  
+    public static void main(String args[])
+
+    {
+
+        Scanner scan=new Scanner(System.in);
+
+      {
+
+         int z;
+
+         System.out.println("Enter first number: ");
+
+         int x=scan.nextInt();
+
+         System.out.println("Enter second number: ");
+
+         int y=scan.nextInt();
+
+         z=x+y;
+
+         System.out.println("Addition of numbers is "+z);
+
+      }
+
+    }
+
+}
